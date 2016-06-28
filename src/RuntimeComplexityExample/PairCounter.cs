1b229455ff7cb9296bf556233a5393f5ac8b5097
Actias <rex@actias.com>
2016-06-27 23:01:09 -0700

    Fixed a bug in the CountPairsNew function x and y should have been swapped.

---
+++ src/RuntimeComplexityExample/PairCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace RuntimeComplexityExample
+{
+    public class PairCounter
+    {
+        // Original Function runtime complexity O(n*n) -squared. Doing the little 2 in a .cs file is bleh.
+
+        // Bug 1) Function was not public so the function could not be reached from outside the class. This wouldn't necessary be a problem except that this is the only function in the class so having a class with a private function is a bit odd.
+
+        // Bug 2) Changed the name of the function to match C# naming conventions and changed the class name since they can't be the same. Since I'm doing this in my native language, I'm going to be picky.
+
+        // Bug 3) The declaration of int a was wrong. The code: int a[] is invalid.
+        public static int CountPairs(int[] a, int x)
+        {
+            var count = 0;
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                for (var j = 0; j < a.Length; j++)
+                {
+                    // Bug 4) This wouldn't have worked right because the if was comparing the loop cursors and not the actual values from the array.
+                    if (a[i] + a[j] == x)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        //New algorithm should be O(n)
+        public static int CountPairsNew(int[] a, int x)
+        {
+            return a.Intersect(a.Select(y => x - y)).Count();
+        }
+    }
+}
--- src/RuntimeComplexityExample/Program.cs
+++ src/RuntimeComplexityExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,61 @@ namespace RuntimeComplexityExample
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello there. This little app will compar two functions speed to count the pairs of numbers that add up to a given number");
+
+            while (true)
+            {
+                Console.WriteLine("How big would you like the array (0 to n)?");
+
+                var input = Console.ReadLine();
+
+                int arraySize;
+                int numberToFind;
+
+                if (!int.TryParse(input, out arraySize))
+                    Console.WriteLine("No...just...no...");
+
+                Console.WriteLine("Cool, now what number would you like to look for?");
+
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out numberToFind))
+                    Console.WriteLine("No...just...no...");
+
+                Console.WriteLine("Alright! Let's do this thing! Hit Enter to get this party started.");
+                Console.ReadLine();
+
+                var array = Enumerable.Range(0, arraySize).ToArray();
+
+                Console.WriteLine("Testing CountPairs (the original function)...");
+
+                var sw = Stopwatch.StartNew();
+
+                var pairs = PairCounter.CountPairs(array, numberToFind);
+
+                sw.Stop();
+
+                Console.WriteLine("Number of Pairs: {0}", pairs);
+                Console.WriteLine("Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+
+                Console.WriteLine("Testing CountPairs1 (the new function)...");
+
+                sw = Stopwatch.StartNew();
+
+                pairs = PairCounter.CountPairsNew(array, numberToFind);
+
+                sw.Stop();
+
+                Console.WriteLine("Number of Pairs: {0}", pairs);
+                Console.WriteLine("Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+
+
+                Console.WriteLine("Fun times right? Test again?");
+
+                if (Console.ReadLine().ToLower().StartsWith("y")) continue;
+
+                break;
+            }
         }
     }
 }
