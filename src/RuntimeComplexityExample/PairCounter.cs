daefe4172e46223a61db80e582c1ceec40a25e23
actias <actias@users.noreply.github.com>
2016-07-01 10:13:10 -0700

    Update PairCounter.cs

--- src/RuntimeComplexityExample/PairCounter.cs
+++ src/RuntimeComplexityExample/PairCounter.cs
@@ -6,7 +6,7 @@ namespace RuntimeComplexityExample
     {
         // Original Function runtime complexity O(n*n) -squared. Doing the little 2 in a .cs file is bleh.
 
-        // Bug 1) Function was not public so the function could not be reached from outside the class. This wouldn't necessary be a problem except that this is the only function in the class so having a class with a private function is a bit odd.
+        // Bug 1) Function was not public so the function could not be reached from outside the class. This wouldn't necessarily be a problem except that this is the only function in the class so having a class with a private function is a bit odd.
 
         // Bug 2) Changed the name of the function to match C# naming conventions and changed the class name since they can't be the same. Since I'm doing this in my native language, I'm going to be picky.
 
